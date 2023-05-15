fdb5455e02e06ecff1df4b80d4432db543be2baf
Rizu <88640976+Zeptiana@users.noreply.github.com>
2023-05-15 13:29:20 +0700

    Add simple_stack in python

---
+++ simple_stack.py
@@ -0,0 +1,15 @@
+stack = [1,2,3,4,5]
+print ("Tumpukan Sekarang : ", stack)
+
+stack.append(6)
+print ("Tumpukan Masuk : ", 6)
+print ("Tumpukan Sekarang : ", stack)
+
+stack.append(7)
+print ("Tumpukan Masuk : ", 7)
+print ("Tumpukan Sekarang : ", stack)
+
+out = stack.pop()
+print ("Tumpukan Keluar : ", out)
+print ("Tumpukan Sekarang : ", stack)
+
